f1c72a01f45501f0668843547c2a65a13f98e52e
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-17 14:18:16 -0700

    Add PreFarmStage

--- settings.py
+++ settings.py
@@ -1,1 +1,3 @@
 MAX_RETRIES = 3 # Number of times to run qdo recover when pending tasks reaches 0
+OUTPUT_DIR_PREFIX = "dr9"
+SCRATCH_DIR = "/global/cscratch1/sd/ziyaoz/"
--- stages.py
+++ stages.py
@@ -1,3 +1,8 @@
+from settings import *
+import subprocess
+import qdo
+import sqlite3
+
 class Stage:
     def __init__(self, name, previous_stage):
         self.name = name
@@ -28,3 +33,30 @@ class SentinelStage(Stage):
 
     def print_status():
         print('I am the sentinel. I am always 100% complete.')
+
+class PreFarmStage(Stage):
+    def __init__(self, name, previous_stage):
+        self.queue = qdo.connect('name')
+        return super().__init__(name, previous_stage)
+    
+    def add_tasks(self):
+        """No need to add tasks
+        Bricks that needs to be processed are manually stored in a qdo queue
+        named self.name
+        """
+        pass
+    
+    def schedule_jobs(self):
+        pass
+
+    def is_done(self):
+        n_pending = self.queue.status()['ntasks']['Pending']
+        n_running = self.queue.status()['ntasks']['Running']
+        return ((n_running == 0 and n_pending == 0) or
+            (n_pending == 0 and get_current_retries() >= MAX_RETRIES))
+
+    def get_current_retries(self):
+        conn = sqlite3.connect('sdo.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM retries WHERE stage=?', self.name)
+        print(c.fetchone())
