8549d7a1a31e007a63f96785c4c42b6879c64811
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-18 20:49:41 -0700

    Make script debug friendly

--- stages.py
+++ stages.py
@@ -86,15 +86,18 @@ class QdoCentricStage(Stage):
                 ((runnning_tasks == 0 and pending_tasks == 0) or finished_last_retry))
 
     def get_jobs_in_queue(self):
-        command = 'sacct -X -P --format=JobId,JobName%-30,Elapsed,State'
-        output = subprocess.run(command, stdout=subprocess.PIPE,
-                                stdin=subprocess.PIPE, shell=True).stdout
-        output = output.decode('utf-8')
-        csv_r = csv.DictReader(io.StringIO(str), delimiter='|')
+        # command = 'squeue -u ziyaoz --format=%all'
+        # output = run_command(command)
+        # Use dummy output for debugging purposes
+        output = """ACCOUNT|TRES_PER_NODE|MIN_CPUS|MIN_TMP_DISK|END_TIME|FEATURES|GROUP|OVER_SUBSCRIBE|JOBID|NAME|COMMENT|TIME_LIMIT|MIN_MEMORY|REQ_NODES|COMMAND|PRIORITY|QOS|REASON||ST|USER|RESERVATION|WCKEY|EXC_NODES|NICE|S:C:T|JOBID|EXEC_HOST|CPUS|NODES|DEPENDENCY|ARRAY_JOB_ID|GROUP|SOCKETS_PER_NODE|CORES_PER_SOCKET|THREADS_PER_CORE|ARRAY_TASK_ID|TIME_LEFT|TIME|NODELIST|CONTIGUOUS|PARTITION|PRIORITY|NODELIST(REASON)|START_TIME|STATE|UID|SUBMIT_TIME|LICENSES|CORE_SPEC|SCHEDNODES|WORK_DIR
+                desi|craynetwork:1|1|0|2019-07-18T19:13:59|haswell|ziyaoz|NO|23159825|sh|(null)|4:00:00|118G||(null)|0.00001609348692|interactive|None||R|ziyaoz|(null)|(null)||0|*:*:*|23159825|mom5|64|1||23159825|79963|*|*|*|N/A|3:53:54|6:06|nid00105|0|interactive|69121|nid00105|2019-07-18T15:13:59|RUNNING|79963|2019-07-18T15:13:59|(null)|N/A|(null)|/global/u2/z/ziyaoz
+                desi|craynetwork:1|1|0|N/A|haswell|ziyaoz|NO|23160238|ziyao-big-blobs|(null)|1-16:00:00|0||/global/cscratch1/sd/landriau/dr8/code/qdo/qdo/etc/qdojob-shifter|0.00001508858987|regular_1|Priority||PD|ziyaoz|(null)|(null)||0|*:*:*|23160238|n/a|1|1||23160238|79963|*|*|*|N/A|1-16:00:00|0:00||0|regular|64805|(Priority)|N/A|PENDING|79963|2019-07-18T15:42:40|(null)|N/A|(null)|/global/u2/z/ziyaoz"""
+        csv_r = csv.DictReader(io.StringIO(output), delimiter='|')
         jobs = []
         for row in csv_r:
-            if row['JobName'] == self.name:
-                jobs.append(row)
+            # Disable name checking for debugging purposes
+            # if row['JobName'] == self.name:
+            jobs.append(row)
         return jobs
 
     def get_current_retries(self):
@@ -175,3 +178,4 @@ class PreFarmStage(QdoCentricStage):
                 '--script "{3}"')
             command = command.format(self.name, self.cores_per_worker, hrs, SDO_DIR, script_path)
             output = run_command(command)
+            print(output)
