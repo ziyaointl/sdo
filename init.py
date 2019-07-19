d1eb1d7e4cbd2229a51a8e31f5071de759fcff7d
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-18 20:50:11 -0700

    Initialize scripts from templates

--- init.py
+++ init.py
@@ -1,7 +1,24 @@
 import sqlite3
+import os
+from settings import *
 
+# Initialize database
 conn = sqlite3.connect('sdo.db')
 c = conn.cursor()
 c.execute('CREATE TABLE retries (stage VARCHAR NOT NULL, times INT)')
 conn.commit()
 conn.close()
+
+# Initialize scripts
+if not os.path.exists('scripts'):
+    os.makedirs('scripts')
+
+# Read templates
+fin = open('templates/runbrick/runbrick-shifter.sh', 'r')
+runbrick_script = fin.read()
+fin.close()
+
+# Write scripts
+fout = open('scripts/prefarm.sh')
+fout.write(runbrick_script.format(LEGACY_SURVEY_DIR, 17, 'srcs'))
+fout.close()
