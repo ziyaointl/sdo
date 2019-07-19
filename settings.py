102a60300eae872c07254333be54d6fc362475d5
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-18 20:46:15 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 MAX_RETRIES = 3 # Number of times to run qdo recover when pending tasks reaches 0
 OUTPUT_DIR_PREFIX = "dr9"
-SCRATCH_DIR = "/global/cscratch1/sd/ziyaoz/"
+LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/farm-playground"
+SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo"
