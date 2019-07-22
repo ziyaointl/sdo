2cbea61de9f0253ccba7fdbdecfcb76773cb762c
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-22 11:05:41 -0700

    Add stages to main

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from settings import *
 from stages import *
 
 def main():
-    stages = []
     sentinel = SentinelStage()
-    prefarm = PreFarmStage('sdo-prefarm-test', sentinel)
-    stages = [prefarm]
+    prefarm = PreFarmStage(PREFARM_QNAME, sentinel, 8)
+    farm = FarmStage(FARM_QNAME, prefarm, 5)
+    postfarm = PostFarmStage(POSTFARM_QNAME, farm, 12)
+    postfarm_scavenger_one = PostFarmScavengerStage(POSTFARM_SCAVENGER_ONE_QNAME,
+        postfarm, 6, job_duration=4, cores_per_worker=32, arch='haswell')
+    postfarm_scavenger_two = PostFarmScavengerStage(POSTFARM_SCAVENGER_TWO_QNAME,
+        postfarm_scavenger_one, 2, job_duration=4, cores_per_worker=64, arch='haswell')
+    stages = [prefarm, farm, postfarm, postfarm_scavenger_one, postfarm_scavenger_two]
 
     for s in stages:
         if s.is_done():
@@ -16,4 +21,4 @@ def main():
         s.schedule_jobs()
         s.print_status()
 
-# main()
+main()
