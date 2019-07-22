5897ca3d245935a7ca21b7d13ba0b3409828d646
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-22 14:21:56 -0700

    Make transfer_queue less chatty

--- qdo_util.py
+++ qdo_util.py
@@ -11,6 +11,6 @@ def transfer_queue(target_q, source_q, source_state):
             to_be_transferred.add(t.task)
             print(t)
     target_q.add_multiple(to_be_transferred)
-    print('Source Queue:', source_q)
-    print('Target Queue:', target_q)
+    print('Source Queue:', source_q.name)
+    print('Target Queue:', target_q.name)
     print('Transferred', len(to_be_transferred), 'tasks')
