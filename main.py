e9d839efd22b42207cde82ad8a7f16fd632fe8b2
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-25 16:37:22 -0700

    Make less optmistic estimates

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     farm = FarmStage(FARM_QNAME, prefarm, 8)
     postfarm = PostFarmStage(POSTFARM_QNAME, farm, 12)
     postfarm_scavenger_one = PostFarmScavengerStage(POSTFARM_SCAVENGER_ONE_QNAME,
-        postfarm, 6, job_duration=4, cores_per_worker=32, arch='haswell')
+        postfarm, 4, job_duration=4, cores_per_worker=32, arch='haswell')
     postfarm_scavenger_two = PostFarmScavengerStage(POSTFARM_SCAVENGER_TWO_QNAME,
-        postfarm_scavenger_one, 2, job_duration=4, cores_per_worker=64, arch='haswell')
+        postfarm_scavenger_one, 1, job_duration=6, cores_per_worker=64, arch='haswell')
     stages = [prefarm, farm, postfarm, postfarm_scavenger_one, postfarm_scavenger_two]
 
     for s in stages:
