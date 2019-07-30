3625d0623141f88b988f9114529b25639a11212e
Ziyao Zhang <ziyaointl@gmail.com>
2019-07-30 15:11:34 -0700

    Move rendering code to plot() loop

--- dashboard.py
+++ dashboard.py
@@ -13,56 +13,66 @@ def plot(stages):
     """
     if len(stages) == 0:
         raise RuntimeError("Stage list empty")
-    total_tasks = stages[0].queue.tasks()
+    env = Environment(loader=FileSystemLoader('reports/templates'))
+    template = env.get_template('index.html')
     for s in stages:
-        # Plot tasks and their states
-        # Print the number of retries & done?
-        # Print a table of associated tasks
-        # Optional: include logs
-        return
+        # Initialize variables
+        queue_state = s.queue.status()['ntasks']
+        jobs = s.get_jobs_in_queue()
+        tasks = s.queue.tasks()
+        queue_name = s.name
 
-env = Environment(
-    loader=FileSystemLoader('reports/templates')
-)
-template = env.get_template('index.html')
+        # Status Plot
+        states, counts = zip(*queue_state.items())
+        colors = ['#f2e38d', '#4390ca', '#32bdb2', '#99d593', '#d53e4f']
+        source = ColumnDataSource(data=dict(states=states, counts=counts, color=colors))
+        hover = HoverTool(tooltips=[('State', '@states'), ('Count', '@counts')])
+        p = figure(x_range=states,
+                    y_range=(0, max(counts)*1.5),
+                    plot_height=400,
+                    title=queue_name,
+                    toolbar_location=None,
+                    tools=[hover],
+                    sizing_mode='scale_width')
+        p.vbar(x='states', top='counts', color='color', width = 0.9, source=source)
+        p.min_border = 50
+        p.xgrid.grid_line_color = None
 
-# Dummy variables
-queue_state = {'Pending': 35, 'Running': 10, 'Succeeded': 20, 'Failed': 2}
-jobs = [{'name': 'prefarm.sh', 'duration': '2:00:00'}]*30
-tasks = [{'name': '1234p567', 'state': 'Running'},
-        {'name': '1334p567', 'state': 'Pending'},
-        {'name': '1434p567', 'state': 'Failed'}]*10
-queue_name = "Prefarm"
+        # Jobs
+        name, jobid, time_limit, qos, nodes, used_time, state, submit_time, work_dir = zip(*[(j['NAME'], j['JOBID'], j['TIME_LIMIT'], j['QOS'], j['NODES'], j['TIME'], j['STATE'], j['SUBMIT_TIME'], j['WORK_DIR']) for j in jobs])
+        d = {}
+        for column in ['name', 'jobid', 'time_limit', 'qos', 'nodes', 'used_time', 'state', 'submit_time', 'work_dir']:
+            d[column] = locals()[column]
+        source = ColumnDataSource(data=d)
+        columns = [
+                TableColumn(field="jobid", title="ID"),
+                TableColumn(field="name", title="Name"),
+                TableColumn(field="state", title="State"),
+                TableColumn(field="time_limit", title="Time Limit"),
+                TableColumn(field="used_time", title="Used Time"),
+                TableColumn(field="qos", title="QOS"),
+                TableColumn(field="nodes", title="Nodes"),
+                TableColumn(field="submit_time", title="Submit Time"),
+                TableColumn(field="work_dir", title="Working Directory")
+        ]
+        jobs_table = DataTable(source=source, columns=columns, height=200, sizing_mode='stretch_both')
 
-# Status Plot
-states, counts = zip(*queue_state.items())
-colors = ['#4390ca', '#32bdb2', '#99d593', '#d53e4f']
-source = ColumnDataSource(data=dict(states=states, counts=counts, color=colors))
-hover = HoverTool(tooltips=[('State', '@states'), ('Count', '@counts')])
-p = figure(x_range=states, y_range=(0, max(counts)*1.5), plot_height=400, title=queue_name, toolbar_location=None, tools=[hover], sizing_mode='scale_width')
-p.vbar(x='states', top='counts', color='color', width = 0.9, source=source)
-p.min_border = 50
-p.xgrid.grid_line_color = None
+        # Tasks
+        name, state = zip(*[(t.task, t.state) for t in tasks])
+        source = ColumnDataSource(data=dict(name=name, state=state))
+        columns = [
+                TableColumn(field="name", title="Name"),
+                TableColumn(field="state", title="State")
+        ]
+        tasks_table = DataTable(source=source, columns=columns, height=200, sizing_mode='stretch_both')
 
-# Jobs
-name, duration = zip(*[(j['name'], j['duration']) for j in jobs])
-source = ColumnDataSource(data=dict(name=name, duration=duration))
-columns = [
-        TableColumn(field="name", title="Name"),
-        TableColumn(field="duration", title="Duration")
-]
-jobs_table = DataTable(source=source, columns=columns, height=200, sizing_mode='stretch_both')
+        script, divs = components((p, jobs_table, tasks_table))
 
-# Tasks
-name, state = zip(*[(t['name'], t['state']) for t in tasks])
-source = ColumnDataSource(data=dict(name=name, state=state))
-columns = [
-        TableColumn(field="name", title="Name"),
-        TableColumn(field="state", title="State")
-]
-tasks_table = DataTable(source=source, columns=columns, height=200, sizing_mode='stretch_both')
-
-script, divs = components((p, jobs_table, tasks_table))
-
-with open('reports/index.html', 'w') as f:
-    f.write(template.render(plot=divs[0], script=script, retries=1, done=False, stage=queue_name, jobs=divs[1], tasks=divs[2]))
+        with open('reports/{}.html'.format(queue_name), 'w') as f:
+            f.write(template.render(plot=divs[0],
+                                    script=script,
+                                    retries=1,
+                                    done=False,
+                                    stage=queue_name,
+                                    jobs=divs[1],
+                                    tasks=divs[2]))
