ad75af08c9a99319619c7fa19a2d0a79cbd6922e
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-01 15:31:05 -0700

    Use time.strftime()

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ from bokeh.models.widgets import DataTable, TableColumn
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import components
-from time import localtime
+from time import strftime
 
 def render(stages):
     """Takes in a list of stages and generates a report in the reports/ directory
@@ -102,7 +102,7 @@ def render(stages):
                                     tasks_table=divs[2],
                                     hours_in_queue=hours(s.get_time_in_queue()),
                                     bokeh=CDN.render(),
-                                    timenow=localtime()
+                                    timenow=strftime('%Y-%m-%d %H:%M:%S')
                                     ))
         print('Written {}.html'.format(queue_name))
 
@@ -136,6 +136,6 @@ def render(stages):
                                 farm_progress = "{0:.2f}".format(succeeded_tasks[1] / total_tasks * 100),
                                 postfarm_progress = "{0:.2f}".format(sum(succeeded_tasks[2:]) / total_tasks * 100),
                                 bokeh=CDN.render(),
-                                timenow=localtime()
+                                timenow=strftime('%Y-%m-%d %H:%M:%S')
                                 ))
     print('Written index.html'.format(queue_name))
