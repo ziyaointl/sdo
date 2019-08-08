540009554476b0da55c3bae5aff0853bd9240b08
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-08 13:23:21 -0700

    Fix y_range

--- dashboard.py
+++ dashboard.py
@@ -45,7 +45,7 @@ def render(stages):
         colors = ['#4390ca', '#32bdb2', '#99d593', '#d53e4f']
         source = ColumnDataSource(data=dict(states=states, counts=counts, color=colors))
         p = figure(x_range=states,
-                    y_range=(0, max(counts)*1.5),
+                    y_range=(0, max(counts)*1.5) or 1,
                     plot_height=400,
                     tools='',
                     toolbar_location=None,
