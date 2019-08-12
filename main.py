28ea4396e930bb1b6080ff933483edee07759af4
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-12 12:22:21 -0700

    Half the cores per worker in pre-farm stage

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dashboard import render
 
 def main():
     sentinel = SentinelStage()
-    prefarm = PreFarmStage(PREFARM_QNAME, sentinel, 8, job_duration=4)
+    prefarm = PreFarmStage(PREFARM_QNAME, sentinel, 8, job_duration=4, cores_per_worker=8)
     farm = FarmStage(FARM_QNAME, prefarm, 4.5)
     postfarm = PostFarmStage(POSTFARM_QNAME, farm, 2)
     postfarm_scavenger_one = PostFarmScavengerStage(POSTFARM_SCAVENGER_ONE_QNAME,
