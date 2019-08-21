841319d4b9103320e9286c4fef5d707fb4c0c652
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-20 23:56:49 -0700

    Update query_time.py

--- helper_scripts/query_time.py
+++ helper_scripts/query_time.py
@@ -1,7 +1,23 @@
 import sqlite3
+import sys 
+import csv
+from io import StringIO
+sys.path.append('..')
+from util import run_command, parse_timedelta
+from datetime import timedelta
+from util import hours
 
-conn = sqlite3.connect('../sdo.db')
+conn = sqlite3.connect('sdo.db')
 c = conn.cursor()
+times = {}
 for row in c.execute('SELECT * FROM jobs'):
-    print(row)
-conn.close()
+    stage, jobid = row[0], row[1]
+    output = run_command('sacct -j {} -l -P'.format(jobid))
+    output = StringIO(output)
+    reader = csv.DictReader(output, delimiter='|')
+    for row in reader:
+        if '.' not in row['JobID']:
+            times[stage] = times.get(stage, timedelta()) + parse_timedelta(row['Elapsed'])
+            print('Parsed', row['JobID'])
+for k, dt in times.items():
+    print(k, hours(dt))
