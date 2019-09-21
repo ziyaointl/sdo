e322affed013e36481f266d577aa549173408180
Ziyao Zhang <ziyaointl@gmail.com>
2019-09-20 17:26:02 -0700

    Fix farm executable location

--- templates/farm/launch-farm.sh
+++ templates/farm/launch-farm.sh
@@ -4,6 +4,7 @@
 # {0} LEGACY_SURVEY_DIR / outdir
 # {1} QNAME
 # {2} SDO_SCRIPT_DIR
+# {3} SDO_DIR
 
 ###Dependencies
 source {2}/mpi_bugfix.sh
@@ -13,7 +14,7 @@ source {2}/qdo_login.sh
 ###OPTIONS
 # FARM_SCRIPT='/src/legacypipe/py/legacypipe/farm.py'
 # This is a temporary solution. Package farm.py with container!
-FARM_SCRIPT='/global/cscratch1/sd/ziyaoz/sdo/farm.py'
+FARM_SCRIPT='{3}/farm.py'
 FARM_OUTDIR='{0}/checkpoints/checkpoint-%(brick)s.pickle'
 FARM_INDIR='{0}/pickles/%(brick).3s/runbrick-%(brick)s-srcs.pickle'
 QNAME='{1}'
