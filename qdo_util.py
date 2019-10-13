bc175d8442bd3bf4720285b357bb040d4b21a5b6
Ziyao Zhang <ziyaointl@gmail.com>
2019-10-13 02:37:34 -0700

    Add condition parameter to transfer_queue()

--- qdo_util.py
+++ qdo_util.py
@@ -1,14 +1,15 @@
 import qdo
 import sqlite3
 
-def transfer_queue(target_q, source_q, source_state):
+def transfer_queue(target_q, source_q, source_state, condition=lambda x: True):
     """If previous stage is QdoCentricStage, add tasks of a certain state
-    from the previous stage's queue to this stage's queue
+    satisfiying a certain condition function from the previous stage's queue
+    to this stage's queue
     """
     to_be_transferred = set([])
     existing = set([t.task for t in target_q.tasks()])
     for t in source_q.tasks():
-        if t.state == source_state and t.task not in existing:
+        if t.state == source_state and t.task not in existing and condition(t.task):
             to_be_transferred.add(t.task)
     target_q.add_multiple(to_be_transferred)
     print('Transferred', len(to_be_transferred), 'tasks')
