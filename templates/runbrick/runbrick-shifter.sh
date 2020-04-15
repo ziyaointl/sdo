876fac099ccda6a8fb31501843c10290bf3201ea
Mark Zhang <ziyaointl@gmail.com>
2020-04-14 18:43:56 -0700

    Bump LSLGA version to 6.0

--- templates/runbrick/runbrick-shifter.sh
+++ templates/runbrick/runbrick-shifter.sh
@@ -22,7 +22,7 @@ export UNWISE_MODEL_SKY_DIR=/global/cfs/cdirs/cosmo/work/wise/unwise_catalog/dr2
 export GAIA_CAT_DIR=/global/cfs/projectdirs/cosmo/work/gaia/chunks-gaia-dr2-astrom-2
 export GAIA_CAT_VER=2
 export TYCHO2_KD_DIR=/global/cfs/projectdirs/cosmo/staging/tycho2
-export LARGEGALAXIES_CAT=/global/cfs/projectdirs/cosmo/staging/largegalaxies/v5.0/LSLGA-model-v5.0.kd.fits
+export LARGEGALAXIES_CAT=/global/cfs/projectdirs/cosmo/staging/largegalaxies/v6.0/LSLGA-model-v6.0.kd.fits
 export PS1CAT_DIR=/global/cfs/projectdirs/cosmo/work/ps1/cats/chunks-qz-star-v3
 
 export PYTHONPATH=/usr/local/lib/python:/usr/local/lib/python3.6/dist-packages:/src/unwise_psf/py:.
