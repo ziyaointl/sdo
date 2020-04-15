21a0e3d581a45221072b2e82998874ad5741b92d
Mark Zhang <ziyaointl@gmail.com>
2020-04-14 19:22:01 -0700

    Replace projectdirs with cdirs

--- templates/runbrick/runbrick-shifter.sh
+++ templates/runbrick/runbrick-shifter.sh
@@ -14,16 +14,16 @@
 export LEGACY_SURVEY_DIR={0}
 BB=${LEGACY_SURVEY_DIR}/
 
-BLOB_MASK_DIR=/global/cfs/projectdirs/cosmo/work/legacysurvey/dr8/{3}
-export DUST_DIR=/global/cfs/projectdirs/cosmo/data/dust/v0_1
-export UNWISE_COADDS_DIR=/global/cfs/projectdirs/cosmo/work/wise/outputs/merge/neo5/fulldepth:/global/cfs/projectdirs/cosmo/data/unwise/allwise/unwise-coadds/fulldepth
-export UNWISE_COADDS_TIMERESOLVED_DIR=/global/cfs/projectdirs/cosmo/work/wise/outputs/merge/neo5
+BLOB_MASK_DIR=/global/cfs/cdirs/cosmo/work/legacysurvey/dr8/{3}
+export DUST_DIR=/global/cfs/cdirs/cosmo/data/dust/v0_1
+export UNWISE_COADDS_DIR=/global/cfs/cdirs/cosmo/work/wise/outputs/merge/neo5/fulldepth:/global/cfs/cdirs/cosmo/data/unwise/allwise/unwise-coadds/fulldepth
+export UNWISE_COADDS_TIMERESOLVED_DIR=/global/cfs/cdirs/cosmo/work/wise/outputs/merge/neo5
 export UNWISE_MODEL_SKY_DIR=/global/cfs/cdirs/cosmo/work/wise/unwise_catalog/dr2/mod
-export GAIA_CAT_DIR=/global/cfs/projectdirs/cosmo/work/gaia/chunks-gaia-dr2-astrom-2
+export GAIA_CAT_DIR=/global/cfs/cdirs/cosmo/work/gaia/chunks-gaia-dr2-astrom-2
 export GAIA_CAT_VER=2
-export TYCHO2_KD_DIR=/global/cfs/projectdirs/cosmo/staging/tycho2
-export LARGEGALAXIES_CAT=/global/cfs/projectdirs/cosmo/staging/largegalaxies/v6.0/LSLGA-model-v6.0.kd.fits
-export PS1CAT_DIR=/global/cfs/projectdirs/cosmo/work/ps1/cats/chunks-qz-star-v3
+export TYCHO2_KD_DIR=/global/cfs/cdirs/cosmo/staging/tycho2
+export LARGEGALAXIES_CAT=/global/cfs/cdirs/cosmo/staging/largegalaxies/v6.0/LSLGA-model-v6.0.kd.fits
+export PS1CAT_DIR=/global/cfs/cdirs/cosmo/work/ps1/cats/chunks-qz-star-v3
 
 export PYTHONPATH=/usr/local/lib/python:/usr/local/lib/python3.6/dist-packages:/src/unwise_psf/py:.
 
