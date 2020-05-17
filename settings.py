3faefdffb75cf3e3147f9db2063af0cd19c0409d
Ziyao Zhang <ziyaointl@gmail.com>
2020-05-16 23:52:04 -0700

    Add settings for dr9i

--- settings.py
+++ settings.py
@@ -3,13 +3,13 @@
 
 import os
 MAX_RETRIES = 1 # Number of times to run qdo recover when pending tasks reaches 0
-LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr9g/north"
-SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr9g-north"
-IMAGE_TAG = "dr9.3.4"
+LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr9i/north"
+SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr9i-north"
+IMAGE_TAG = "DR9.4.1"
 SDO_SCRIPT_DIR = os.path.join(SDO_DIR, 'scripts')
 QDO_LOGIN_PATH = '/global/cscratch1/sd/ziyaoz/farm/qdo_login.sh'
 TELESCOPE = 'north' # north or north
-PUBLIC_REPORT_PATH = '/global/project/projectdirs/desi/www/users/ziyaoz/dr9g/north'
+PUBLIC_REPORT_PATH = '/global/project/projectdirs/desi/www/users/ziyaoz/dr9i/north'
             # In addition to storing reports to reports/history and reports/current,
             # also copy them to this location
 SUMMARY_FORMAT = 'simple' # Choice between classic and simple
@@ -17,14 +17,14 @@ HASWELL_MEM = 125000000
 KNL_MEM = 93750000
 
 # Queue names
-PREFIX='ziyao-dr9g-north-'
+PREFIX='ziyao-dr9i-north-'
 PREFARM_QNAME = PREFIX + 'prefarm'
 PREFARM_SCAVENGER_ONE_QNAME = PREFIX + 'prefarm-scavenger-1'
 PREFARM_SCAVENGER_TWO_QNAME = PREFIX + 'prefarm-scavenger-2'
 FARM_QNAME = PREFIX + 'farm'
-POSTFARM_QNAME = PREFIX + 'postfarm'
-POSTFARM_SCAVENGER_ONE_QNAME = PREFIX + 'postfarm-scavenger-1'
-POSTFARM_SCAVENGER_TWO_QNAME = PREFIX + 'postfarm-scavenger-2'
+POSTFARM_QNAME = PREFIX + '1'
+POSTFARM_SCAVENGER_ONE_QNAME = PREFIX + '2'
+POSTFARM_SCAVENGER_TWO_QNAME = PREFIX + '3'
 
 # Allocations
 KNL_ACCT = 'm3592'
