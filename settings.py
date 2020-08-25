9ac6aaf535e458ad8f1ab7715101076047ed8d89
Ziyao Zhang <ziyaointl@gmail.com>
2020-08-25 02:39:12 -0700

    dr9m-south

--- settings.py
+++ settings.py
@@ -3,22 +3,22 @@
 
 import os
 MAX_RETRIES = 1 # Number of times to run qdo recover when pending tasks reaches 0
-LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr9m/north"
-SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr9m-north"
-IMAGE_TAG = "DR9.6.2"
+LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr9m/south"
+SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr9m-south"
+IMAGE_TAG = "DR9.6.4"
 SDO_SCRIPT_DIR = os.path.join(SDO_DIR, 'scripts')
 QDO_LOGIN_PATH = '/global/cscratch1/sd/ziyaoz/farm/qdo_login.sh'
-TELESCOPE = 'north'
-PUBLIC_REPORT_PATH = '/global/cfs/cdirs/desi/users/ziyaoz/dr9m/north'
+TELESCOPE = 'south'
+PUBLIC_REPORT_PATH = '/global/cfs/cdirs/desi/users/ziyaoz/dr9m/south'
             # In addition to storing reports to reports/history and reports/current,
             # also copy them to this location
 SUMMARY_FORMAT = 'simple' # Choice between classic and simple
 HASWELL_MEM = 125000000
 KNL_MEM = 93750000
-EXTRA_PARAMS = '--release 9011' # Additional parameter to be passed to runbrick ONLY
+EXTRA_PARAMS = '--release 9010' # Additional parameter to be passed to runbrick ONLY
 
 # Queue names
-PREFIX='ziyao-dr9m-north-'
+PREFIX='ziyao-dr9m-south-'
 PREFARM_QNAME = PREFIX + 'prefarm'
 PREFARM_SCAVENGER_ONE_QNAME = PREFIX + 'prefarm-scavenger-1'
 PREFARM_SCAVENGER_TWO_QNAME = PREFIX + 'prefarm-scavenger-2'
@@ -28,8 +28,8 @@ POSTFARM_SCAVENGER_ONE_QNAME = PREFIX + '2'
 POSTFARM_SCAVENGER_TWO_QNAME = PREFIX + '3'
 
 # Allocations
-KNL_ACCT = 'm3592'
-HASWELL_ACCT = 'm3592'
+KNL_ACCT = 'desi'
+HASWELL_ACCT = 'desi'
 
 # Burst Buffer
 # How to use:
