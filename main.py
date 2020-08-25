f58040d2e97850f60ad9a8d85625c39ce41eee30
Ziyao Zhang <ziyaointl@gmail.com>
2020-08-25 02:34:12 -0700

    Reduce estimate for north-2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     #prefarm_scavenger_one = PreFarmScavengerStage(PREFARM_SCAVENGER_ONE_QNAME, prefarm, 8, job_duration=4, cores_per_worker=17)
     #prefarm_scavenger_two = PreFarmScavengerStage(PREFARM_SCAVENGER_TWO_QNAME, prefarm_scavenger_one, 1/6, job_duration=12, cores_per_worker=17)
     #farm = FarmStage(FARM_QNAME, prefarm_scavenger_two, 4)
-    postfarm = PostFarmStage(POSTFARM_QNAME, sentinel, 4, cores_per_worker=17, arch='knl', max_nodes_per_job=5)
+    postfarm = PostFarmStage(POSTFARM_QNAME, sentinel, 3.5, cores_per_worker=17, arch='knl', max_nodes_per_job=40)
     postfarm_scavenger_one = PostFarmScavengerStage(POSTFARM_SCAVENGER_ONE_QNAME,
         postfarm, 1/4, job_duration=4, cores_per_worker=34, arch='knl', max_nodes_per_job=40)
     postfarm_scavenger_two = PostFarmScavengerStage(POSTFARM_SCAVENGER_TWO_QNAME,
