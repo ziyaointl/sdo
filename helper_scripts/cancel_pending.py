43855800360091472ad08cf1b9ea99470d4b8e35
Ziyao Zhang <ziyaointl@gmail.com>
2020-09-12 20:39:58 -0700

    Finish implementing cancel_pending

--- helper_scripts/cancel_pending.py
+++ helper_scripts/cancel_pending.py
@@ -1,6 +1,11 @@
 import sys
 sys.path.append("..")
 from stages import *
+from util import run_command
 
 sentinel = SentinelStage()
-print(sys.argv)
+stage = RunbrickPyStage(sys.argv[1], sentinel, 0)
+for j in stage.get_jobs_in_queue():
+    if j['STATE'] == 'PENDING':
+        id = j['JOBID']
+        run_command("scancel {}".format(j['JOBID']))
