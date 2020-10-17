50a9bc678a7fcc31d12a73b10dd6866c1d89b5ee
Ziyao Zhang <ziyaointl@gmail.com>
2020-10-17 01:59:30 -0700

    Make stages declarative

--- main.py
+++ main.py
@@ -1,50 +1,15 @@
 from settings import *
 from stages import *
+from stage_instances import stage_instances
 from dashboard import render
 from init import init
 
 def main():
-    """Here, cores_per_worker indicates how many cores are allocated to each worker.
-    For the actual number of cores (processes) used, refer to init.py
+    """Executes each stage and renders report
     """
     init()
-    sentinel = SentinelStage()
-    #prefarm = PreFarmStage(PREFARM_QNAME, sentinel, 16, job_duration=4, cores_per_worker=8)
-    #prefarm_scavenger_one = PreFarmScavengerStage(PREFARM_SCAVENGER_ONE_QNAME, prefarm, 8, job_duration=4, cores_per_worker=17)
-    #prefarm_scavenger_two = PreFarmScavengerStage(PREFARM_SCAVENGER_TWO_QNAME, prefarm_scavenger_one, 1/6, job_duration=12, cores_per_worker=17)
-    #farm = FarmStage(FARM_QNAME, prefarm_scavenger_two, 4)
-    postfarm = PostFarmStage(
-        POSTFARM_QNAME,
-        sentinel,
-        tasks_per_nodehr=3.5,
-        cores_per_worker=17,
-        job_duration=4,
-        arch='knl',
-        max_nodes_per_job=80
-    )
-    postfarm_scavenger_one = PostFarmScavengerStage(
-        POSTFARM_SCAVENGER_ONE_QNAME,
-        postfarm,
-        tasks_per_nodehr=1/4,
-        job_duration=4,
-        cores_per_worker=34,
-        arch='knl',
-        max_nodes_per_job=80
-    )
-    postfarm_scavenger_two = PostFarmScavengerStage(
-        POSTFARM_SCAVENGER_TWO_QNAME,
-        postfarm_scavenger_one,
-        tasks_per_nodehr=1/6,
-        job_duration=6,
-        cores_per_worker=32,
-        arch='haswell',
-        max_nodes_per_job=80
-    )
-
-    stages = [postfarm, postfarm_scavenger_one, postfarm_scavenger_two]
-
     separator_len = 40
-    for s in stages:
+    for s in stage_instances:
         print('='*separator_len)
         print(s.name)
         print('-'*separator_len)
@@ -54,6 +19,6 @@ def main():
         s.schedule_jobs() # Schedule new jobs if needed
         s.print_status()
         print('='*separator_len + '\n')
-    render(stages)
+    render(stage_instances)
 
 main()
--- stage_instances.py
+++ stage_instances.py
@@ -1,21 +1,61 @@
-from stages import *
+from stages import SentinelStage, PostFarmScavengerStage
+
+def gen_stages(stages_def, default_def):
+    """Given stage definitions, generate a list of stage instances
+    """
+    # Merge stage defs with default; if a field is contained in both, the default one is overwritten
+    stages_def = [ {**default_def, **sdef} for sdef in stages_def ]
+
+    stages_dict = {}
+    stages_list = []
+
+    for sdef in stages_def:
+        curr_stage = sdef['class'](
+            sdef['name'],
+            SentinelStage() if sdef['prev_stage'] is None else stages_dict[sdef['prev_stage']],
+            tasks_per_nodehr=sdef['tasks_per_nodehr'],
+            cores_per_worker=sdef['cores_per_worker'],
+            job_duration=sdef['job_duration'],
+            arch=sdef['arch'],
+            max_nodes_per_job=sdef['max_nodes_per_job'],
+        )
+        stages_list.append(curr_stage)
+        stages_dict[sdef['name']] = curr_stage
+
+    return stages_list
 
 prefix = 'ziyao-dr9m-south-'
-stages_def = {
-    prefix + '1': {
+stage_instances = gen_stages(
+    [{
+        'name': prefix + '1',
         'prev_stage': None,
         'tasks_per_nodehr': 3.5,
         'cores_per_worker': 17,
         'job_duration': 4,
         'arch': 'knl',
-        'max_nodes_per_job': 80
+        'max_nodes_per_job': 80,
     },
-    prefix + '2': {
+    {
+        'name': prefix + '2',
         'prev_stage': prefix + '1',
         'tasks_per_nodehr': 1/4,
         'job_duration': 4,
         'cores_per_worker': 34,
         'arch': 'knl',
-        'max_nodes_per_job': 80
+        'max_nodes_per_job': 80,
+    },
+    {
+        'name': prefix + '3',
+        'prev_stage': prefix + '2',
+        'tasks_per_nodehr': 1/6,
+        'job_duration': 6,
+        'cores_per_worker': 32,
+        'arch': 'haswell',
+        'max_nodes_per_job': 80,
+    }],
+    {
+        'allocation': 'desi', # TODO
+        'queue': 'regular', # TODO
+        'class': PostFarmScavengerStage,
     }
-}
+)
