785475f6506365f6a476d3ff29cee624c8e29f1f
Ziyao Zhang <ziyaointl@gmail.com>
2022-04-26 23:09:35 -0700

    Update settings.py for dr10

--- settings.py
+++ settings.py
@@ -5,12 +5,12 @@ import os
 
 ### User defined fields
 MAX_RETRIES = 1 # Number of times to run qdo recover when pending tasks reaches 0
-LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr9m2/south"
-SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr9m2-south"
-IMAGE_TAG = "docker:legacysurvey/legacypipe:DR9.6.8" # "docker:legacysurvey/legacypipe:DR9.6.7"
-QDO_LOGIN_PATH = '/global/cscratch1/sd/ziyaoz/farm/qdo_login.sh'
+LEGACY_SURVEY_DIR = "/global/cscratch1/sd/ziyaoz/dr10/south"
+SDO_DIR = "/global/cscratch1/sd/ziyaoz/sdo-dr10"
+IMAGE_TAG = "docker:legacysurvey/legacypipe:DR9.10.1"
+QDO_LOGIN_PATH = '/global/homes/z/ziyaoz/qdo.sh'
 TELESCOPE = 'south'
-PUBLIC_REPORT_PATH = '/global/cfs/cdirs/desi/users/ziyaoz/dr9m2/south'
+PUBLIC_REPORT_PATH = '/global/cfs/cdirs/desi/users/ziyaoz/dr10/south'
             # In addition to storing reports to reports/history and reports/current,
             # also copy them to this location
 EXTRA_PARAMS = '--release 9010' # Additional parameter to be passed to runbrick ONLY
